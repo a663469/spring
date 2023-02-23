326f4111936431f0f7eafb0566b817dfb0954f9f
Sabr <a663469@gmail.com>
2023-02-23 21:26:35 +0300

    test After

--- defPrj/src/main/java/aop/aspects/UniversityLoggingAspect.java
+++ defPrj/src/main/java/aop/aspects/UniversityLoggingAspect.java
@@ -1,10 +1,7 @@
 package aop.aspects;
 
 import aop.Student;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -12,11 +9,12 @@ import java.util.List;
 @Component
 @Aspect
 public class UniversityLoggingAspect {
-    @Before("execution(* getStudents())")
-    public void beforeGetStudentsLoggingAdvice() {
-        System.out.println("beforeGetStudentsLoggingAdvice: логируем полученние" +
-                "списка студентов перед методом getStudents");
-    }
+//    @Before("execution(* getStudents())")
+//    public void beforeGetStudentsLoggingAdvice() {
+//        System.out.println("beforeGetStudentsLoggingAdvice: логируем полученние" +
+//                "списка студентов перед методом getStudents");
+//    }
+
 //    @AfterReturning(pointcut = "execution(* getStudents())", returning = "students")
 //    public void afterReturningGetStudentsLoggingAdvice(List<Student> students) {
 //        Student firstStudent = students.get(0);
@@ -34,8 +32,14 @@ public class UniversityLoggingAspect {
 //    }
 
 
-    @AfterThrowing(pointcut = "execution(* getStudents())", throwing = "exception")
-    public void afterThrowingGetStudentsLoggingAdvice(Throwable exception){
-        System.out.println("beforeThrowingGetStudentsLoggingAdvice: логируем выброс исключения " + exception);
-    }
+//    @AfterThrowing(pointcut = "execution(* getStudents())", throwing = "exception")
+//    public void afterThrowingGetStudentsLoggingAdvice(Throwable exception){
+//        System.out.println("beforeThrowingGetStudentsLoggingAdvice: логируем выброс исключения " + exception);
+//    }
+
+   @After("execution(* getStudents())")
+   public void  afterGetStudentsLoggingAdvice(){
+       System.out.println("afterGetStudentsLoggingAdvice: логируем нормальное окончание работы метода или выброса" +
+               " исключения");
+   }
 }

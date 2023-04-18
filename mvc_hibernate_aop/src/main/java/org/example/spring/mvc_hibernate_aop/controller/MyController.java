c4cf16ac78747c788e8bc2f5198e6d8840a12bed
Sabr <a663469@gmail.com>
2023-04-18 23:38:26 +0300

    Changing and deleting an employee

--- mvc_hibernate_aop/src/main/java/org/example/spring/mvc_hibernate_aop/controller/MyController.java
+++ mvc_hibernate_aop/src/main/java/org/example/spring/mvc_hibernate_aop/controller/MyController.java
@@ -1,9 +1,6 @@
 package org.example.spring.mvc_hibernate_aop.controller;
 
-
-
 import com.sun.org.apache.xpath.internal.operations.Mod;
-import org.example.spring.mvc_hibernate_aop.dao.EmployeeDAO;
 import org.example.spring.mvc_hibernate_aop.entity.Employee;
 import org.example.spring.mvc_hibernate_aop.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -43,6 +41,20 @@ public class MyController {
         System.out.println("saveEmployee!!");
         employeeService.saveEmployee(employee);
 
+        return "redirect:/";
+    }
+
+    @RequestMapping("/updateInfo")
+    public String updateEmployee(@RequestParam("empId") int id, Model model) {
+
+        Employee employee = employeeService.getEmployee(id);
+        model.addAttribute("employee", employee);
+
+        return "employee-info";
+    }
+    @RequestMapping("/deleteEmployee")
+    public String deleteEmployee(@RequestParam("empId") int id) {
+        employeeService.deleteEmployee(id);
         return "redirect:/";
     }
 }
--- mvc_hibernate_aop/src/main/java/org/example/spring/mvc_hibernate_aop/dao/EmployeeDAO.java
+++ mvc_hibernate_aop/src/main/java/org/example/spring/mvc_hibernate_aop/dao/EmployeeDAO.java
@@ -1,6 +1,7 @@
 package org.example.spring.mvc_hibernate_aop.dao;
 
 import org.example.spring.mvc_hibernate_aop.entity.Employee;
+import org.hibernate.Session;
 
 import java.util.List;
 
@@ -8,4 +9,8 @@ public interface EmployeeDAO {
     public List<Employee> getAllEmployees();
 
     public void saveEmployee(Employee employee);
+
+    public Employee getEmployee(int id);
+
+    public void deleteEmployee(int id);
 }

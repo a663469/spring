74a86d580ce0a5203dee12038a79c25a44f5a25d
Sabr <a663469@gmail.com>
2023-10-01 23:15:09 +0300

    Test actuator

---
+++ spring-data-rest/src/main/java/org/example/spring/springboot/spring_data_rest/dao/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package org.example.spring.springboot.spring_data_rest.dao;
+
+
+import org.example.spring.springboot.spring_data_rest.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+}
